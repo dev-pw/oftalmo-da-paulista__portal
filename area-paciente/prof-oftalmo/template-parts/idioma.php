08342ceeae58a9aab7287cf44b482ee1a88de8a2
Alexandre <alexandrescrideli@gmail.com>
2025-11-14 11:46:24 -0300

    add .

--- area-paciente/prof-oftalmo/template-parts/idioma.php
+++
@@ -1,5 +0,0 @@
-<div class="row gx-2">
-    <div class="col-auto pt"><?php echo do_shortcode( '[gt-link lang="pt"]' )?></div>
-    <div class="col-auto en"><?php echo do_shortcode( '[gt-link lang="en"]' )?></div>
-    <div class="col-auto esp"><?php echo do_shortcode( '[gt-link lang="es"]' )?></div>
-</div>
